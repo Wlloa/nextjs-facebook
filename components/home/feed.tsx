a5b0ccc53a850f67be7655af498e342d05bb0861
Wilber Ulloa Jorge <wuj890312@gmail.com>
2021-10-11 22:43:27 -0400

    created historie cards (#4)

--- components/home/feed.tsx
+++ components/home/feed.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import { StyledProps } from "../../common/props-interface";
+import { HISTORIES } from "../../models/history";
+import { HistoryCards } from "./history-cards";
 import { PostList, PostsProp } from "./postList";
 
 const Feed = (props: PostsProp): JSX.Element => {
   const { posts, className } = props;
   return (
     <div className={className}>
+      <HistoryCards histories={HISTORIES.slice(0, 5)}/>
       <PostList posts={posts} />
     </div>
   );
 };
 
 export const MainFeed = styled(Feed)`
-  margin-top: 56px;
+  padding: 56px 32px 0;
   min-height: calc(100vh - 56px);
   overflow-y: auto;
   display: flex;
@@ -21,10 +24,9 @@ export const MainFeed = styled(Feed)`
   flex-basis: 744px;
   max-width: 500px;
   align-items: stretch;
-  padding: 0 32px;
   flex-grow: 1;
   z-index: 0;
   @media(min-width: 760px) {
-      max-width: 680px;
+      max-width: 744px;
   }
 `;
