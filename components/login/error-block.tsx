ed51b10199c712b63bfc1e4ca27ca83beba0eb01
Wlloa <wuj890312@gmail.com>
2021-10-22 17:57:15 -0400

    added close to error block

--- components/login/error-block.tsx
+++ components/login/error-block.tsx
@@ -1,44 +1,65 @@
-import React from 'react'
-import styled from 'styled-components'
-import { StyledProps } from '../../common/props-interface'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { StyledProps } from "../../common/props-interface";
 
 export interface ErrorProps extends StyledProps {
-    title?: string;
-    text?: string;
+  title?: string;
+  text?: string;
 }
 
-const ErrorBlock = (props: ErrorProps) => {
-    const {title, text, className} = props
-    return (
-        <div className={className}>
-            {title && <h3>{title}</h3>}
-            {text && <p>{text}</p>}
-        </div>
-    )
-}
+const ErrorBlock = (props: ErrorProps): JSX.Element => {
+  const { title, text, className } = props;
+  const [showBlock, setShowBlock] = useState(true);
+
+  const dismiss = () => {
+    setShowBlock(false);
+  };
+
+  if(!showBlock) {
+      return null;
+  }
+
+  return (
+    <div className={className}>
+      <span onClick={dismiss}>x</span>
+      {title && <h3>{title}</h3>}
+      {text && <p>{text}</p>}
+    </div>
+  );
+};
 
 export const StyledErrorBlock = styled(ErrorBlock)`
-    background-color: #ffebe8;
-    border: 1px solid #dd3c10;
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-     h3 {
-         color: #333;
-         font-weight: 700;
-         font-size: 14px;
-         text-align: center;
-         line-height: 1.34;
-     }
+  background-color: #ffebe8;
+  border: 1px solid #dd3c10;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  h3 {
+    color: #333;
+    font-weight: 700;
+    font-size: 14px;
+    text-align: center;
+    line-height: 1.34;
+  }
 
-     p{
-        font-size: 12px;
-        color: #1c1e21;
-        line-height: 1.34;
-     }
+  p {
+    font-size: 12px;
+    color: #1c1e21;
+    line-height: 1.34;
+  }
 
-     @media (max-width: 767px){
-         margin: 0 16px;
-     }
+  span {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+    font-weight: 700;
+    color: #333;
+    cursor: pointer;
+  }
+
+  @media (max-width: 767px) {
+    margin: 0 16px;
+  }
 `;
