2d6cca77745f8277e60719a8c89d13470d3192bb
Wlloa <wuj890312@gmail.com>
2021-11-11 22:21:32 -0500

    udpated history model

--- models/history.ts
+++ models/history.ts
@@ -1,42 +1,44 @@
 import { IPost } from "./post";
 
-export interface IHistory extends Required<Omit<IPost, "date" | "description">> {}
+export interface IHistory extends Required<Omit<IPost, "date" | "description" | "timestamp" | "user">> {}
 
 export const HISTORIES: IHistory[] = [
   {
     id: "1",
     userName: "Wil",
-    userPicture: "/static/miscellanea/me.jpg",
+    userImage: "/static/miscellanea/me.jpg",
     postPicture: "/static/miscellanea/fra-bel.jpg",
+
+
   },
   {
     id: "2",
     userName: "Wil",
-    userPicture: "/static/miscellanea/me.jpg",
+    userImage: "/static/miscellanea/me.jpg",
     postPicture: "/static/miscellanea/fra-bel.jpg",
   },
   {
     id: "3",
     userName: "Wil",
-    userPicture: "/static/miscellanea/me.jpg",
+    userImage: "/static/miscellanea/me.jpg",
     postPicture: "/static/miscellanea/fra-bel.jpg",
   },
   {
     id: "4",
     userName: "Wil",
-    userPicture: "/static/miscellanea/me.jpg",
+    userImage: "/static/miscellanea/me.jpg",
     postPicture: "/static/miscellanea/fra-bel.jpg",
   },
   {
     id: "5",
     userName: "Wil",
-    userPicture: "/static/miscellanea/me.jpg",
+    userImage: "/static/miscellanea/me.jpg",
     postPicture: "/static/miscellanea/fra-bel.jpg",
   },
   {
     id: "6",
     userName: "Wil",
-    userPicture: "/static/miscellanea/me.jpg",
+    userImage: "/static/miscellanea/me.jpg",
     postPicture: "/static/miscellanea/fra-bel.jpg",
   },
 ];
